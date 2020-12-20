c87cfe245ded815b8b84555601bda4cbfbd82f75
AbdallahHussien4 <Abdalah.Abdelhameed98@eng-st.cu.edu.eg>
2020-12-20 03:17:38 +0200

    Finish Final Ram
    Finish work after dictionary

--- main.py
+++ main.py
@@ -41,6 +41,10 @@ def DetectVariables(line):
     global Address    
     x=False  
     Pc=""
+    if(re.fullmatch('JSR',line[0])):
+        Pc=line[2]
+        x=True
+        return line,x,Pc
     for var in Variables:
         if(len(line)>2):
             if(re.fullmatch(var,line[1]) and not re.fullmatch('DEFINE',line[0]) ):
@@ -74,13 +78,15 @@ def GetAddresses(Lines):
 
         #Detect Variables 
         FormattedLine,Check,Pc=DetectVariables(SplittedLine)
+
         if(FormattedLine[0].find('DEFINE')!=-1):
             LabelVariables[FormattedLine[1]]={"Address":Address,"Value":FormattedLine[2]}
 
         if(Check):
-            if(FormattedLine[0].find('DEFINE')==-1 and re.findall("\d", Pc)):
+            if(FormattedLine[0].find('DEFINE')==-1 and FormattedLine[0].find('JSR')==-1 and re.findall("\d", Pc)):
                 ImportantLines.append(FormattedLine)
                 ImportantLines.append(int(Pc))
+
             elif(FormattedLine[0].find('DEFINE')==-1):
                 ImportantLines.append(FormattedLine)
                 ImportantLines.append(Pc)    
@@ -94,8 +100,127 @@ def ReplaceVariables(lines):
     for line in range(0,len(lines)):
         if (not isinstance(lines[line], list) and isinstance(lines[line], str)): 
             lines[line]=LabelVariables[lines[line]]['Address']-line
-    return lines        
+    return lines  
 
+def DetectModes(str):
+    if(re.fullmatch("\(R[0-9]\)\+",str)):
+        return "autoIncrement"
+    if(re.fullmatch("\-\(R[0-9]\)",str)):
+        return "autoDecrement"
+    if(re.fullmatch("R[0-9]",str)):
+        return "direct"
+    if(re.fullmatch("[^@-]\(R[0-9]\)",str)):
+        return "indexed"
+    if(re.fullmatch("@\(R[0-9]\)\+",str)):
+        return "autoIncrementIndirect"
+    if(re.fullmatch("@\-\(R[0-9]\)",str)):
+        return "autoDecrementIndirect"
+    if(re.fullmatch("@R[0-9]",str)):
+        return "directIndirect"
+    if(re.fullmatch("@[^-]+\(R[0-9]\)",str)):
+        return "indexedIndirect" 
+
+def DetectRegister(str):
+    x=re.search('R',str)
+    first=x.span()[0]
+    x=str[first:first+2]
+    return x
+
+
+get_bin = lambda x, n: format(x, 'b').zfill(n)
+def GenerateOutPut(lines):
+    f = open("testOut.txt", "w")
+    for line in range(0,len(lines)):
+        if (isinstance(lines[line], list)):
+            if(lines[line][0] in Instructions.keys()):
+                f.write(Instructions[lines[line][0]])
+            else:
+                print("Errooor!!")
+            if(len(lines[line])==1):
+                continue
+            elif(len(lines[line])==2):
+                if(re.fullmatch(("^B.*$"),lines[line][0])):
+                    Offset=LabelVariables[lines[line][1]]['Address']-line
+                    Offset=Offset & 0x3ff
+                    f.write(get_bin(Offset, 10))
+                else:
+                    f.write(Modes[DetectModes(lines[line][1])])
+                    f.write(Registers[DetectRegister(lines[line][1])])    
+                    f.write('0000')
+
+            elif(len(lines[line])==3):
+                f.write(Modes[DetectModes(lines[line][1])])
+                f.write(Registers[DetectRegister(lines[line][1])]) 
+
+                if(re.fullmatch(("JSR"),lines[line][0])):
+                    Offset=LabelVariables[lines[line][1]]['Address']-line
+                    Offset=Offset & 0xffff
+                    f.write('0000\n')
+                    f.write(get_bin(Offset, 16))
+                else:    
+                    f.write(Modes[DetectModes(lines[line][2])])
+                    f.write(Registers[DetectRegister(lines[line][2])])    
+        else:
+            f.write(get_bin(lines[line]& 0xffff, 16))                       
+            
+        f.write('\n')         
+
+Registers={
+    "R0":"000",
+    "R1":"001",
+    "R2":"010",
+    "R3":"011",
+    "R4":"100",
+    "R5":"101",
+    "R6":"110",
+    "R7":"111",
+}
+Modes={
+    "direct":"000",
+    "autoIncrement":"001",
+    "autoDecrement":"010",
+    "indexed":"011",
+    "directIndirect":"100",
+    "autoIncrementIndirect":"101",
+    "autoDecrementIndirect":"110",
+    "indexedIndirect":"111",
+}
+Instructions={
+    "MOV":"0000",
+    "ADD":"0001",
+    "ADC":"0010",
+    "SUB":"0011",
+    "SBC":"0100",
+    "AND":"0101",
+    "OR":"0110",
+    "XOR":"0111",
+    "CMP":"1000",
+    "INC":"100100",
+    "DEC":"100101",
+    "CLR":"100110",
+    "INV":"100111",
+    "LSR":"101000",
+    "ROR":"101001",
+    "ASR":"101010",
+    "LSL":"101011",
+    "ROL":"101100",
+    "BR":"110000",
+    "BEQ":"110001",
+    "BNE":"110010",
+    "BLO":"110011",
+    "BLS":"110100",
+    "BHI":"110101",
+    "BHS":"110110",
+    "HLT":"1110000000000000",
+    "NOP":"1110010000000000",
+    "RESET":"1110100000000000",
+    "JSR":"111100",
+    "RTS":"111101",
+    "INT":"1111100000000000",
+    "IRET":"1111100000000000",
+    
+
+}
 Address=0    
 txt=ReadFile("BlaBla")
 LinesNoComments=[]
@@ -107,4 +232,5 @@ ImportantLines=[]
 NoComma=[]
 GetAddresses(LinesNoComments)
 SemiFinalRam=ReplaceVariables(ImportantLines)
-print(SemiFinalRam)
+GenerateOutPut(SemiFinalRam)
+
--- registersModes.py
+++ registersModes.py
@@ -1,12 +1,12 @@
 import re
-str="@SP"
+str="R1"
 str=str.upper()
 str=str.replace("SP","R6")
 str=str.replace("PC","R7")
-autoIncrement=re.fullmatch("\((R[0-9])\)\+",str)
+autoIncrement=re.fullmatch("\(R[0-9]\)\+",str)
 autoDecrement=re.fullmatch("\-\(R[0-9]\)",str)
-direct=re.fullmatch("R[0-9]$",str)
-indexed=re.fullmatch("[^@-+]\(R[0-9]\)",str)
+direct=re.fullmatch("R[0-9]",str)
+indexed=re.fullmatch("[^@-]\(R[0-9]\)",str)
 autoIncrementIndirect=re.fullmatch("@\(R[0-9]\)\+",str)
 autoDecrementIndirect=re.fullmatch("@\-\(R[0-9]\)",str)
 directIndirect=re.fullmatch("@R[0-9]",str)
